07886ff415fd9399e2d7e97d1db20d649d25675a
shirameirovitz <shirameirovi@gmail.com>
2021-03-03 20:25:30 +0200

    classes changes

--- api/shorturl.js
+++ api/shorturl.js
@@ -1,57 +1,58 @@
-//shortening the URL
 const express = require("express");
 const shortid = require("shortid");
-const router = require("router");
 const validUrl = require("valid-url");
 const config = require("config");
-const shortUrlRoute = express.Router();
+
 const url = require("../dataBase");
 
 const dataBase = url.DataBase();
 
 function checkIfUrlExists(dataBase, currentUrl) {
-    for(let i = 0; i < dataBase.length; i++) {
-        if( dataBase[i].originalUrl === currentUrl ) {
-            dataBase[i].count += 1;
-            return dataBase[i].shortUrl;
-        }
+  for (let i = 0; i < dataBase.length; i++) {
+    if (dataBase[i].originalUrl === currentUrl) {
+      dataBase[i].count += 1;
+      return dataBase[i].shortUrl;
     }
+  }
+  return false;
 }
-shortUrlRoute.post("/", async (req, res) => {
-    const longUrl = req.body.longUrl;
-    const baseUrl = config.get("baseURL");
+dataBase.post("/new", async (req, res) => {
+  const longUrl = req.body.longUrl;
+  const baseUrl = config.get("api/shorturl/new");
 
-    //check if the url is valid
-    if ( !validUrl.isUri(baseUrl) ) {
-        return res.status(401).json("Internal error. Please come back later.");
+  if (!validUrl.isUri(baseUrl)) {
+    return res.status(401).json("Internal error. Please come back later.");
+  }
+
+  //make an original url id
+  const urlCode = shortid.generate();
+
+  //check if the original url is valid
+  if (validUrl.isUri(longUrl)) {
+    try {
+      const urlExists = await checkIfUrlExists(dataBase, longUrl);
+      //check if url exists - if not adds new url to dataBase
+      if (!urlExists) {
+        const shortUrl = baseUrl + "/" + urlCode;
+        urlExists = dataBase.addUrl(longUrl, shortUrl);
+        return res.status(201).json(urlExists);
+      } else {
+        //if url exists return exists shortUrl
+        return res.status(200).json(urlExists);
+      }
+    } catch (err) {
+      //server error
+      console.error(err.message);
+      return res.status(500).json("Internal Server error " + err.message);
     }
-
-    const urlCode = shortid.generate();
-
-    if ( validUrl.isUri(longUrl) ) {
-
-        try {
-            const urlExists = await checkIfUrlExists(dataBase ,longUrl);
-            if (url) {
-                return  res.status(200).json(urlExists);
-            } else {
-
-                const shortUrl = baseUrl + "/" + urlCode;
-                urlExists  = new Url({});
-                
-                await urlExists.save()
-                return res.status(201).json(urlExists);
-            }
-        } catch(err) {
-            console.error(err.message);
-            return res.status(500).json("Internal Server error " + err.message);
-        }
-    } else {
-        res.status(400).json("Invalid URL. Please enter a valid url for shortening.");
-    }    
+  } else {
+    res
+      .status(400)
+      .json("Invalid URL. Please enter a valid url for shortening.");
+  }
 });
 
-module.exports = shortUrlRoute;
+module.exports = dataBase;
 // route.post("/new", async (req, res) => {
 //     const longUrl = req.body.longUrl;
 //     const baseUrl = config.get("baseURL");
