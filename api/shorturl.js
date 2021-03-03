011e1ba7d05a636866e7798deafb0b61f47a5713
shirameirovitz <shirameirovi@gmail.com>
2021-03-03 20:53:01 +0200

    change short url

--- api/shorturl.js
+++ api/shorturl.js
@@ -1,55 +1,73 @@
-const  = require("express");
+const express = require("express");
 const shortid = require("shortid");
-const router = require("router");
 const validUrl = require("valid-url");
-const config = require("config");
 const url = require("../dataBase");
 
-const dataBase = url.DataBase();
+const router = express.Router();
+router.use(express.json());
+router.use(express.urlencoded());
+const dataBase = url.DataBase;
+const DB = new dataBase();
 
-function checkIfUrlExists(dataBase, currentUrl) {
-    for(let i = 0; i < dataBase.length; i++) {
-        if( dataBase[i].originalUrl === currentUrl ) {
-            dataBase[i].count += 1;
-            return dataBase[i].shortUrl;
-        }
-    }
-    return false;
-}
-dataBase.post("/", async (req, res) => {
-    const longUrl = req.body.longUrl;
-    const baseUrl = config.get("api/shorturl/new");
-
-    if ( !validUrl.isUri(baseUrl) ) {
-        return res.status(401).json("Internal error. Please come back later.");
-    }
-
-    //make an original url id
-    const urlCode = shortid.generate();
-
-    //check if the original url is valid
-    if ( validUrl.isUri(longUrl) ) {
-
-        try {
-            const urlExists = await checkIfUrlExists(dataBase ,longUrl);
-            //check if url exists - if not adds new url to dataBase
-            if (!urlExists) {
-                const shortUrl = baseUrl + "/" + urlCode;
-                urlExists  = dataBase.addUrl(longUrl, shortUrl);
-                return res.status(201).json(urlExists);
-            } else {
-                //if url exists return exists shortUrl
-                return res.status(200).json(urlExists);
-            }
-
-        } catch(err) {
-            //server error
-            console.error(err.message);
-            return res.status(500).json("Internal Server error " + err.message);
-        }
+router.get("/:id", (request, response) => {
+  try {
+    const { id } = request.params;
+    if (validUrl.isUri(id)) {
+      response.status(200).json({ id });
     } else {
-        res.status(400).json("Invalid URL. Please enter a valid url for shortening.");
-    }    
+      response.status(400).json({
+        message: "Invalid URL. Please enter a valid url for shortening.",
+      });
+    }
+    // let body = fs.readFileSync(`./backend/bins/${id}.json`, {encoding:'utf8', flag:'r'});
+    // body = JSON.parse(body);
+    // response.status(200).json(body);
+  } catch (e) {
+    response
+      .status(500)
+      .json({ message: "Internal Server Error!", error: `${e}` });
+  }
 });
 
-module.exports = shorturl;
+router.post("/new", (request, response) => {
+  try {
+    const { url } = request.body;
+    //make an original url id
+    const urlCode = shortid.generate();
+    DB.addUrl(url, urlCode);
+    response
+      .status(200)
+      .json({ original_url: `${url}`, short_url: `${urlCode}` });
+  } catch (e) {
+    response
+      .status(500)
+      .json({ message: "Internal Server Error!", error: `${e}` });
+  }
+});
+
+//   //check if the original url is valid
+//   if (validUrl.isUri(longUrl)) {
+//     try {
+//       const urlExists = await checkIfUrlExists(dataBase, longUrl);
+//       //check if url exists - if not adds new url to dataBase
+//       if (!urlExists) {
+//         const shortUrl = baseUrl + "/" + urlCode;
+//         urlExists = dataBase.addUrl(longUrl, shortUrl);
+//         return res.status(201).json(urlExists);
+//       } else {
+//         //if url exists return exists shortUrl
+//         return res.status(200).json(urlExists);
+//       }
+//     } catch (err) {
+//       //server error
+//       console.error(err.message);
+//       return res.status(500).json("Internal Server error " + err.message);
+//     }
+//   } else {
+//     res.status(400).json({
+//       message: "Invalid URL. Please enter a valid url for shortening.",
+//     });
+//   }
+// });
+
+module.exports = { router };
