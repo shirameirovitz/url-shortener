123b70438e5622f0e4b056c4209ba2cffcdc5310
shirameirovitz <shirameirovi@gmail.com>
2021-03-04 16:13:01 +0200

    test error for get

--- tests/myTest.test.js
+++ tests/myTest.test.js
@@ -1,17 +1,42 @@
-const app = require("../app")
+const app = require("../app");
 const request = require("supertest");
 const fs = require("fs");
+const { items } = require("../dataBase");
+
+describe("POST route", () => {
+  //   const originalUrl = "https://www.youtube.com/";
+
+  it("should post a new short id successfully", async () => {
+    const response = await request(app)
+      .post("/api/shorturl/new")
+      .type("form")
+      .send({ url: "https://www.youtube.com/" });
+
+    expect(response.status).toBe(200);
+  });
+});
 
 describe("GET route", () => {
-    const item =
-        [
-            {
-        originalUrl: "https://github.com/",
-        shortUrl: "qj8VT-kKy",
-        count: 3,
-        date: "2021-03-04 10:08:17" 
-            }
-        ];
-    const expected
+  const expectedItem = [
+    {
+      originalUrl: "https://github.com/",
+      shortUrl: "qj8VT-kKy",
+      count: 3,
+      date: "2021-03-04 10:08:17",
+    },
+  ];
+  const expectedShortIdError = "short ID cannot be found";
 
-})
+  it("should return a OriginalUrl by a short Id", async () => {
+    const response = await request(app).get("/qj8VT-kKy");
+
+    // is the status code 200
+    expect(response.status).toBe(302);
+  });
+  it("should an error with status 404 for not found url", async () => {
+    const response = await request(app).get("/1111");
+
+    //is the status 404
+    expect(response.status).toBe(404);
+  });
+});
